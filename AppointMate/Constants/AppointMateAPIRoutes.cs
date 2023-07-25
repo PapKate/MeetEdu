6475f20d410623533ec1c021dc85613b860ae569
PapKate <papkate.lessons@gmail.com>
2023-07-25 20:13:33 +0300

    Routes have begun

--- AppointMate/Constants/AppointMateAPIRoutes.cs
+++ AppointMate/Constants/AppointMateAPIRoutes.cs
@@ -9,28 +9,69 @@
         /// The base route
         /// <code>api/appointMate</code>
         /// </summary>
-        private const string ControllerRoute = "api/appointMate";
-
-        #region Companies
+        private const string BaseRoute = "api/appointMate";
 
         /// <summary>
-        /// The companies route
-        /// <code>api/appointMate/companies</code>
+        /// The management app route
+        /// <code>api/appointMate/management</code>
         /// </summary>
-        public const string CompaniesRoute = ControllerRoute + "/companies";
+        private const string ManagementRoute = BaseRoute + "/management";
+
+        /// <summary>
+        /// The app route
+        /// <code>api/appointMate/app</code>
+        /// </summary>
+        private const string AppRoute = BaseRoute + "/app";
+
+        #region App
+
+        #region Companies
+        
+        /// <summary>
+        /// The companies route
+        /// <code>api/appointMate/app/companies</code>
+        /// </summary>
+        public const string CompaniesRoute = AppRoute + "/companies";
 
         /// <summary>
         /// The company route
-        /// <code>api/appointMate/companies/1</code>
+        /// <code>api/appointMate/app/companies/1</code>
         /// </summary>
         public const string CompanyRoute = CompaniesRoute + "/{companyId}";
 
         /// <summary>
-        /// The company route for the company with the specified <paramref name="companyId"/>
-        /// <code>api/appointMate/companies/<paramref name="companyId"/></code>
+        /// The route for the company with the specified <paramref name="companyId"/>
+        /// <code>api/appointMate/app/companies/<paramref name="companyId"/></code>
         /// </summary>
         public static string GetCompanyRoute(string companyId) => CompaniesRoute + $"/{companyId}";
 
+        #endregion
+
+        #region Services
+
+        /// <summary>
+        /// The services route
+        /// <code>api/appointMate/app/services</code>
+        /// </summary>
+        public const string ServicesRoute = AppRoute + "/services";
+
+        /// <summary>
+        /// The service route
+        /// <code>api/appointMate/app/services/1</code>
+        /// </summary>
+        public const string ServiceRoute = ServicesRoute + "/{serviceId}";
+
+        /// <summary>
+        /// The route for the service with the specified <paramref name="serviceId"/>
+        /// <code>api/appointMate/app/services/<paramref name="serviceId"/></code>
+        /// </summary>
+        public static string GetServiceRoute(string serviceId) => ServicesRoute + $"/{serviceId}";
+
+        #endregion
+
+        #endregion
+
+        #region Management
         #endregion
     }
 }
