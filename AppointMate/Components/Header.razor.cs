fbaf851ea87b5ce8bbf1c64ed7a57e4e0c0746dc
PapKate <papkate.lessons@gmail.com>
2023-08-12 10:41:11 +0300

    Correct(?) css variables

---
+++ AppointMate/Components/Header.razor.cs
@@ -0,0 +1,46 @@
+﻿using Atom.Blazor.Controls;
+
+using Microsoft.AspNetCore.Components;
+
+namespace AppointMate
+{
+    /// <summary>
+    /// The header component
+    /// </summary>
+    public partial class Header : ForegroundControl
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The app name
+        /// </summary>
+        [Parameter]
+        public string? AppName { get; set; }
+
+        /// <summary>
+        /// The username
+        /// </summary>
+        [Parameter]
+        public string? Username { get; set; }
+
+        /// <summary>
+        /// The user image URL
+        /// </summary>
+        [Parameter]
+        public Uri? UserImageUrl { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Header() : base()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ AppointMate/Constants/CssConstants.cs
@@ -0,0 +1,93 @@
+﻿using Atom.Blazor;
+using Atom.Blazor.Controls;
+using static Atom.Personalization;
+
+using Microsoft.AspNetCore.Components;
+using System.ComponentModel;
+
+namespace AppointMate
+{
+    /// <summary>
+    /// Constants for CSS
+    /// </summary>
+    public static class CssConstants
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The font family
+        /// </summary>
+        public const string FontFamily = "--fontFamily";
+
+        #region ForeColor
+
+        /// <summary>
+        /// The fore color
+        /// </summary>
+        public const string ForeColor = "--foreColor";
+
+        /// <summary>
+        /// The hover fore color
+        /// </summary>
+        public const string HoverForeColor = "--hoverForeColor";
+
+        /// <summary>
+        /// The mouse down fore color
+        /// </summary>
+        public const string MouseDownForeColor = "--mouseDownForeColor";
+
+        /// <summary>
+        /// The is selected fore color
+        /// </summary>
+        public const string SelectedForeColor = "--selectedForeColor";
+
+        /// <summary>
+        /// The disabled fore color
+        /// </summary>
+        public const string DisabledForeColor = "--disabledForeColor";
+
+        #endregion
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Extension methods for <see cref="CssConstants"/>
+    /// </summary>
+    public static class CssConstantExtensions
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Sets the <paramref name="value"/> to the CSS variable with the specified <paramref name="name"/>
+        /// <code>--name: value;</code>
+        /// </summary>
+        /// <param name="name">The name</param>
+        /// <param name="value">The value</param>
+        /// <returns></returns>
+        public static string SetCssVariable(this string name, object value)
+            => $"{name}: {value};";
+
+        /// <summary>
+        /// Sets the <paramref name="value"/> to the CSS variable with the specified <paramref name="name"/>
+        /// <code>--name: value;</code>
+        /// </summary>
+        /// <param name="name">The name</param>
+        /// <param name="value">The value</param>
+        /// <returns></returns>
+        public static string SetCssVariable(this string name, string value)
+            => $"{name}: {(value.IsHexValue() ? value.NormalizedColor() : value)};";
+
+        /// <summary>
+        /// Gets the CSS variable with the specified <paramref name="name"/>
+        /// <code>var(--name)</code>
+        /// </summary>
+        /// <param name="name">The name</param>
+        /// <returns></returns>
+        public static string GetCssVariable(this string name)
+            => $"var({name})";
+
+        #endregion
+    }
+
+}
