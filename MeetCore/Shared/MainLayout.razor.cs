2d9303478b6997ebcabd4c4509887a22126327bd
PapKate <papkate.lessons@gmail.com>
2023-09-10 21:47:12 +0300

    MeetCore back to its initial state

---
+++ MeetCore/Shared/MainLayout.razor.cs
@@ -0,0 +1,41 @@
+﻿using MudBlazor;
+
+namespace MeetCore.Shared
+{
+    public partial class MainLayout
+    {
+        #region Private Members
+
+        /// <summary>
+        /// The theme provider
+        /// </summary>
+        private MudTheme? mMeetCoreTheme;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MainLayout() : base()
+        {
+            mMeetCoreTheme = new MudTheme()
+            {
+                Typography = new Typography()
+                {
+                    Default = new Default()
+                    {
+                        FontFamily = new[] { "Objektiv VF Trial", "sans-serif" }
+                    }
+                },
+                Palette = new()
+                {
+                    
+                }
+            };
+        }
+
+        #endregion
+    }
+}
